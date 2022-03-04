de19a47ff20a6f3833a167318ac1b39f5ab2aaa7
Ana Navarro <anavarro@telecomboutique.com>
2022-03-04 13:24:35 +0100

    initial folder structuring and installation of dependencies

---
+++ mca-spain/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function App() {
+  return <Title>Hello world!</Title>;
+}
+
+export default App;
+
+const Title = styled.span`
+  font-family: Montserrat-Regular;
+`;
